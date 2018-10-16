58634c28419c6f17cad581ab79e957d14da0147a
jakkarn <jakobnorell@gmail.com>
2018-10-16 17:44:31 +0200

    added missing query

--- lab1_1.pl
+++ lab1_1.pl
@@ -52,20 +52,14 @@ likes(peter, X) :- kind(X).
 likes(ulrika, X) :- male(X), rich(X), kind(X), likes(X, ulrika).        % a
 likes(ulrika, X) :- male(X), beautiful(X), strong(X), likes(X, ulrika). % b
 
-:- tell('./lab-1-1-out.txt').
+:- tell('./lab1-1-out.txt').
 :- format("QUERY (who is happy?) 'happy(X)':", []), nl.
 :- findall(X, happy(X), L),
 	(foreach(E, L) do format("~a is happy", [E]), nl).
 :- nl, format("QUERY (who likes whom?) 'likes(X, Y)':", []), nl.
 :- findall((X,Y), likes(X,Y), L),
-	(foreach((X,Y), L) do format("~a likes ~a", [X, Y]), nl).
+	(foreach((X1,Y1), L) do format("~a likes ~a", [X1, Y1]), nl).
+:- nl, format("QUERY (who likes ulrika?) 'likes(X, ultika)':", []), nl.
+:- findall(X, likes(X,ulrika), L),
+	(foreach(E, L) do format("~a likes Ulrika", [E]), nl).
 :- told.
-
-/* OUTPUT */
-% :- write("QUERY> happy(X). (Who is happy?)"), nl.
-% :- forall(happy(X), (write("-: "), write(X), write(" is happy"), nl)).
-% :- nl, write("QUERY> likes(X,Y). (Who likes whom?)"), nl.
-% :- forall(likes(X,Y), (write("-: "), write(X), write(" likes "), write(Y), nl)).
-% :- nl, write("QUERY> likes(X,ulrika). (How many people like ulrika?)"), nl.
-% :- forall(likes(X, ulrika), (write("-: "), write(X), write(" likes ulrika"), nl)).
-% :- nl, halt(0). % halt swipl
