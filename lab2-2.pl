814d9eae24515e0db5863deee1f238eb1b3091d2
jakkarn <jakobnorell@gmail.com>
2018-11-02 14:04:15 +0100

    changed 'middle' to corresponding 'middleX' where X is an integer from 0-3

--- lab2-2.pl
+++ lab2-2.pl
@@ -7,27 +7,27 @@
 middle0(X, [X]).
 middle0(X, [First|Xs]) :-
 	append(Middle, [Last], Xs),
-	middle(X, Middle).
+	middle0(X, Middle).
 
-/* works */
+/* didn't finish nine elem list */
 middle1(X, [X]).
 middle1(X, [First|Xs]) :-
-	middle(X, Middle),
+	middle1(X, Middle),
 	append(Middle, [Last], Xs).
 
 /* works */
 middle2(X, [First|Xs]) :-
 	append(Middle, [Last], Xs),
-	middle(X, Middle).
+	middle2(X, Middle).
 middle2(X, [X]).
 
-/* works */
+/* didn't finish nine elem list */
 middle3(X, [First|Xs]) :-
-	middle(X, Middle),
+	middle3(X, Middle),
 	append(Middle, [_Last], Xs).
 middle3(X, [X]).
 
-:- nl, middle0(X, [4,3,2,1,22,33,44]), format('middle0 says: ~d', [X]).
-:- nl, middle2(Y, [4,3,2,1,22,33,44]), format('middle1 says: ~d', [Y]).
-:- nl, middle1(Z, [4,3,2,1,22,33,44]), format('middle2 says: ~d', [Z]).
-:- nl, middle3(W, [4,3,2,1,22,33,44]), format('middle3 says: ~d', [W]).
+% :- nl, middle0(X, [4,3,2,1,22,33,44]), format('middle0 says: ~d', [X]).
+% :- nl, middle2(Y, [4,3,2,1,22,33,44]), format('middle1 says: ~d', [Y]).
+% :- nl, middle1(Z, [4,3,2,1,22,33,44]), format('middle2 says: ~d', [Z]).
+% :- nl, middle3(W, [4,3,2,1,22,33,44]), format('middle3 says: ~d', [W]).
