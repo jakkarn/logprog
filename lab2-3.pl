182ed54740517d94c6028f5cbb1a2baff11ce526
jakkarn <jakobnorell@gmail.com>
2019-01-04 19:59:02 +0100

    simple execute predicate that calls a variable P

--- lab2-3.pl
+++ lab2-3.pl
@@ -2,10 +2,19 @@
 
 /* commands:   */
 /* skip        */
+%skip
+
 /* set(I, E)   */
+set(I, E).
+
 /* if(B, C, C) */
+%if(B, C, C).
+
 /* while(B, C) */
+while(B, C).
+
 /* seq(C, C)   */
+seq(C, C).
 
 /* id(I)       */
 /* num(N)      */
@@ -17,9 +26,10 @@
 /* arithmetic expressions:
    id(I), num(N), E + E, E - E, E * E, ... */
    
-/* execute(S0, P, Sn)
+/*
+   execute(S0, P, Sn)
 
    if S0 is a list representing the initial binding environment
    for identifiers and P is the command to run, then Sn is the
-   resulting binding environment when the command P is run
-*/
+   resulting binding environment when the command P is run */
+execute(_S0, P, _Sn) :- call(P).
