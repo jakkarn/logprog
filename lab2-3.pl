18bdd7296866d978b78aabe83fc7bab1a92eedf8
jakkarn <jakobnorell@gmail.com>
2019-01-04 18:53:03 +0100

    wrote comments with initial notes for abstract machine

--- lab2-3.pl
+++ lab2-3.pl
@@ -1,2 +1,25 @@
 /* 2.3 - an abstract machine */
 
+/* commands:   */
+/* skip        */
+/* set(I, E)   */
+/* if(B, C, C) */
+/* while(B, C) */
+/* seq(C, C)   */
+
+/* id(I)       */
+/* num(N)      */
+
+/* binding environment for identifiers:
+   [i = n, i2=n2,...]                      */
+/* boolean expressions:
+   tt, ff, E > E, ...                      */
+/* arithmetic expressions:
+   id(I), num(N), E + E, E - E, E * E, ... */
+   
+/* execute(S0, P, Sn)
+
+   if S0 is a list representing the initial binding environment
+   for identifiers and P is the command to run, then Sn is the
+   resulting binding environment when the command P is run
+*/
